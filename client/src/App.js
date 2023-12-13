6d21888140973174f52a2b32c69646d71d4954a9
mireu (Samuel) <103215918+mireua@users.noreply.github.com>
2023-12-13 06:20:55 +0000

    stable 0.3 (API integration, React Routing, dashboard)

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "./index.css";
 import Login from "./Login";
 import Dashboard from "./dashboard/Dashboard";
+import Test from "./BookingComponent";
  
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Login />} />
       <Route path='dashboard' element={<Dashboard />} />
+      <Route path='test' element={<Test />} />
     </Routes>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,18 +1,33 @@
 const express = require('express');
+const axios = require('axios');
 const { connectToMongoDB } = require('./db/conn');
-const app = express()
+const app = express();
 const cors = require('cors');
 const loginRoutes = require('./routes/login');
 const regRoutes = require('./routes/register');
+const fetchData = require('./routes/travelapi/travelapi'); // Update the path accordingly
 
 app.use(express.json());
 app.use(cors());
 
+// API routes
 app.use('/api', loginRoutes);
 app.use('/api', regRoutes);
 
-connectToMongoDB()
+// Example route using fetchData
+app.get('/api/travelapi', async (req, res) => {
+  try {
+    const data = await fetchData();
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-app.listen(5000, () => {
-  console.log(`Server is running on port 5000!`);
+connectToMongoDB();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}!`);
 });
